8f24734e2862c74d4a9a7d8807511bf0872d8c33
zuzuzu <8200514+TaikiSuzuki@users.noreply.github.com>
2025-07-18 19:00:49 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   devise_for :users
-  root to: "messages#index"
+  root to: "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
 
